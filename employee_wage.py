585b865c7c878b836c47f3ed21f7cd81429d41fe
saurabh323351 <singh.saurabh3333@gmail.com>
2020-11-12 01:35:42 +0530

    Added : code to compute daily wage

--- employee_wage.py
+++ employee_wage.py
@@ -6,3 +6,12 @@ import random
 random_no = random.randint(0,1)
 is_present=True if random_no else False
 print(is_present,random_no)
+
+employee_wage_details={
+    "wage_per_hour":20,
+    "full_day_hour":8
+}
+
+compute_daily_wage = lambda wage_per_hour, full_day_hour: wage_per_hour * full_day_hour if is_present else 0
+# print(compute_daily_wage(employee_wage_details.get('wage_per_hour'), employee_wage_details.get('full_day_hour')))
+compute_daily_wage(employee_wage_details.get('wage_per_hour'), employee_wage_details.get('full_day_hour'))
