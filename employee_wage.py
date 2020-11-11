11c37a735ad514a0698c60c5afa2e9b68a28c896
saurabh323351 <singh.saurabh3333@gmail.com>
2020-11-12 00:47:02 +0530

    added is_present flag to handle employee attendance

--- employee_wage.py
+++ employee_wage.py
@@ -1,1 +1,8 @@
 # welcome to employee_wage problem statement
+
+import random
+"""random.randint(0,1) Return random integer in range [a, b], including both end points.
+"""
+random_no = random.randint(0,1)
+is_present=True if random_no else False
+print(is_present,random_no)
